b060c2c978f4b90c8681b7a85bb8f899340d13fd
Mehmet Tekman <mtekman89@gmail.com>
2020-12-30 17:27:13 +0100

    rename to floatmode +1

--- exwm-picture-in-picture.el
+++ exwm-picture-in-picture.el
@@ -1,11 +1,11 @@
-;;; exwm-picture-in-picture.el --- Convenient modes and bindings for
+;;; exwm-floatmode.el --- Convenient modes and bindings for
 ;;; floating picture-in-picture EXWM frames -*- lexical-binding: t;
 ;;; -*-
 
 ;; Copyright (C) 2020 Mehmet Tekman <mtekman89@gmail.com>
 
 ;; Author: Mehmet Tekman
-;; URL: https://gitlab.com/mtekman/exwm-picture-in-picture.el
+;; URL: https://gitlab.com/mtekman/exwm-floatmode.el
 ;; Keywords: outlines
 ;; Package-Requires: ((emacs "25.1") (xelb "0.18") (exwm "0.24") (dash "2.17.0"))
 ;; Version: 0.2
@@ -34,95 +34,164 @@
 (require 'exwm-input)
 (require 'exwm-floating)
 
-(defgroup exwm-picture-in-picture nil
+(defgroup exwm-floatmode nil
   "Customization group for picture-in-picture."
   :group 'exwm-floating)
 
-(defvar exwm-picture-in-picture---float-buffer nil
+(defvar exwm-floatmode--float-buffer nil
   "Buffer currently showed in the floating frame.")
 
-(defcustom exwm-picture-in-picture-move-amount 20
-  "Amount to move the floating frame for the minor mode."
-  :type 'integer
-  :group 'exwm-picture-in-picture)
+(defcustom exwm-floatmode-title "Picture-in-Picture"
+  "Name of floating exwm title to modify."
+  :type 'string
+  :group 'exwm-floatmode)
 
-(defcustom exwm-picture-in-picture-geometry
+(defcustom exwm-floatmode-modify-amount
+  '(:move 20 :resize 50)
+  "Incremental amounts to :MOVE or :RESIZE the floating frame for the minor mode."
+  :type 'plist
+  :group 'exwm-floatmode)
+
+(defcustom exwm-floatmode-geometry
   '(x 0.6 y 0.05 width 600 height 500)
   "Geometry for the floating window when spawned.
 Units can be a fraction of the visible workspace, or integer pixel values."
   :type 'alist
-  :group 'exwm-picture-in-picture)
+  :group 'exwm-floatmode)
 
-(defcustom exwm-picture-in-picture-decorations
+(defcustom exwm-floatmode-decorations
   '(floating-mode-line nil floating-header-line nil
                        tiling-header-line nil tiling-mode-line nil char-mode nil)
   "Decorations for the floating window when spawned."
   :type 'alist
-  :group 'exwm-picture-in-picture)
+  :group 'exwm-floatmode)
 
-(defcustom exwm-picture-in-picture-border
+(defcustom exwm-floatmode-border
   '(:stationary ("navy" . 1) :moving ("maroon" . 2))
   "Floating border and width when stationary and during move-mode."
   :type 'plist
-  :group 'exwm-picture-in-picture)
+  :group 'exwm-floatmode)
 
-(define-minor-mode exwm-picture-in-picture-move-mode
-  "The minor mode for manipulating the exwm Picture-in-Picture frame"
+(defcustom exwm-floatmode
+
+;;(unintern exwm-floatmode-move-mode)
+(define-minor-mode exwm-floatmode-move-mode
+  "The minor mode for manipulating the exwm floating frame. Works only when called on the floating frame, and it should therefore only be called from the ``exwm-floatmode-minor-mode'' function, which ensures this."
   :init-value nil
-  :lighter " Picture-in-Picture"
+  :lighter " FloatMode"
   :keymap
-  '(([left] . (lambda () (interactive) (exwm-picture-in-picture--move 'left)))
-    ([right] . (lambda () (interactive) (exwm-picture-in-picture--move 'right)))
-    ([up] . (lambda () (interactive) (exwm-picture-in-picture--move 'up)))
-    ([down] . (lambda () (interactive) (exwm-picture-in-picture--move 'down)))
-    ([? ] . exwm-picture-in-picture-toggle-video)
-    ([return] . exwm-picture-in-picture-move-mode))
-  (let* ((mode (if exwm-picture-in-picture-move-mode :moving :stationary))
-         (vald (plist-get exwm-picture-in-picture-border mode)))
+  '(([left] . (lambda () (interactive) (exwm-floatmode-move 'left)))
+    ([right] . (lambda () (interactive) (exwm-floatmode-move 'right)))
+    ([up] . (lambda () (interactive) (exwm-floatmode-move 'up)))
+    ([down] . (lambda () (interactive) (exwm-floatmode-move 'down)))
+    ([1] . (lambda () (interactive) (exwm-floatmode-move 'down)))
+    ([? ] . exwm-floatmode-toggle-video)
+    ([\M-left] . (lambda () (interactive) (exwm-floatmode-resize-delta -20 nil)))
+    ([\M-right] . (lambda () (interactive) (exwm-floatmode-resize-delta 20 nil)))
+    ([\M-up] . (lambda () (interactive) (exwm-floatmode-resize-delta nil -20)))
+    ([\M-down] . (lambda () (interactive) (exwm-floatmode-resize-delta nil 20)))
+    ([return] . exwm-floatmode-move-mode))
+  (when exwm--floating-frame
+    (exwm-floatmode-move-mode -1)
+    ;; For some reason the floating window does not respond to mode
+    ;; commands, and when enabled on the buffer it shifts the buffer
+    ;; out of the floating frame.
+    (user-error "This mode should NOT be enabled on the floating frame!"))
+  (let* ((mode (if exwm-floatmode-move-mode :moving :stationary))
+         (vald (plist-get exwm-floatmode-border mode)))
     (customize-set-variable 'exwm-floating-border-color (car vald))
     (customize-set-variable 'exwm-floating-border-width (cdr vald))))
 
-(defun exwm-picture-in-picture--get-floating-buffer ()
+(defvar exwm-floatmode--prewindow nil
+  "Window before minor-mode was called, to be restored on exit.")
+
+(defun exwm-floatmode-minor-mode-disable ()
+  "Disable minor mode."
+  (interactive)
+  (exwm-floatmode-move-mode -1)
+  ;;(remove-hook 'exwm-floating-exit-hook #'exwm-floatmode-minor-mode-disable)
+  (message "What UP!"))
+
+(defun exwm-floatmode-minor-mode (&optional state)
+  "Parent caller for ``exwm-floatmode-move-mode''.
+Selects the floating window and sets the minor mode to STATE, 1 for on, anything else for off."
+  (interactive)
+  (let ((state (or state 1))
+        (fwin (exwm-floatmode--get-floating-window)))
+    (unless fwin (user-error "No floating frame detected"))
+    (if (eq state 1) ;; mode start
+        (progn (setq exwm-floatmode--prewindow
+                     (get-buffer-window (current-buffer)))
+               (select-window fwin)
+               (exwm-floatmode-move-mode t)
+               (add-hook 'exwm-floating-exit-hook
+                         #'exwm-floatmode-minor-mode-disable))
+      ;; mode exit
+      (select-window fwin)
+      (exwm-floatmode-move-mode -1)
+      ;; restore previous window
+      (remove-hook 'exwm-floating-exit-hook #'exwm-floatmode-minor-mode-disable)
+      (select-window exwm-floatmode--prewindow))))
+
+(defun exwm-floatmode--get-floating-buffer ()
   "Get the floating frame buffer.
 If not found or currently active, search for it and update it."
-  (if (buffer-live-p exwm-picture-in-picture---float-buffer)
-      exwm-picture-in-picture---float-buffer
+  (if (buffer-live-p exwm-floatmode--float-buffer)
+      exwm-floatmode--float-buffer
     ;; Attempt to find new buffer
     (dolist (buff (buffer-list)
-                  (if (buffer-live-p exwm-picture-in-picture---float-buffer)
-                      exwm-picture-in-picture---float-buffer))
+                  (if (buffer-live-p exwm-floatmode--float-buffer)
+                      exwm-floatmode--float-buffer))
       (if (with-current-buffer buff exwm--floating-frame)
-          (setq exwm-picture-in-picture---float-buffer buff)))))
+          (setq exwm-floatmode--float-buffer buff)))))
 
-(defun exwm-picture-in-picture--get-floating-window ()
+(defun exwm-floatmode--get-floating-window ()
   "Get the window of the floating buffer."
-  (--> (exwm-picture-in-picture--get-floating-buffer)
+  (--> (exwm-floatmode--get-floating-buffer)
        (if it (get-buffer-window it t))))
 
-(defun exwm-picture-in-picture--get-floating-frame ()
+(defun exwm-floatmode--get-floating-frame ()
   "Get the frame of the floating buffer."
-  (--> (exwm-picture-in-picture--get-floating-window)
+  (--> (exwm-floatmode--get-floating-window)
        (if it (window-frame it))))
 
-(defun exwm-picture-in-picture--hide-tab-bar-in-floating-frame ()
+(defun exwm-floatmode--hide-tab-bar-in-floating-frame ()
   "Hide the tab-bar mode in the floating frame.
 Added to the ``after-make-frame-functions'' hook."
-  (set-frame-parameter (exwm-picture-in-picture--get-floating-frame)
+  (set-frame-parameter (exwm-floatmode--get-floating-frame)
                        'tab-bar-lines 0))
 
-;;;###autoload
-(defun exwm-picture-in-picture-setup ()
-  "Setup the floating window properties and associate it with the ``Picture-in-Picture'' window.  Call this function upon loading the package."
-  (interactive)
-  (pushnew `((equal exwm-title "Picture-in-Picture")
-             floating t
-             ,@exwm-picture-in-picture-decorations
-             ,@exwm-picture-in-picture-geometry)
-           exwm-manage-configurations)
-  (add-hook 'exwm-floating-setup-hook
-            #'exwm-picture-in-picture--hide-tab-bar-in-floating-frame))
+(defun exwm-floatmode--do-floatfunc-and-restore (func)
+  "Select the floating window, perform FUNC and then restore the current window. 
+If the floating window is already selected, then just run FUNC."
+  (let ((curr-win (get-buffer-window (current-buffer) t))
+        (float-win (exwm-floatmode--get-floating-window)))
+    (unless float-win
+      ;; Disable mode-mode if it's enabled
+      (if exwm-floatmode-move-mode (exwm-floatmode-move-mode -1))
+      (user-error "No floating window available"))
+    (if (eq curr-win float-win)
+        (funcall func curr-win float-win)
+      (select-window float-win)
+      (funcall func curr-win float-win)
+      (select-window curr-win))))
 
-(defun exwm-picture-in-picture--mouse-click (&optional pos button-num window-id)
+;;;###autoload
+(defun exwm-floatmode-setup ()
+  "Setup the floating window properties and associate it with the floating window.  Call this function upon loading the package."
+  (interactive)
+  (customize-set-variable 'exwm-manage-configurations
+                          (pushnew
+                           `((equal exwm-title exwm-floatmode-title)
+                             floating t
+                             ,@exwm-floatmode-decorations
+                             ,@exwm-floatmode-geometry)
+                           exwm-manage-configurations))
+  ;; Disable the tab-bar in this frame
+  (add-hook 'exwm-floating-setup-hook
+            #'exwm-floatmode--hide-tab-bar-in-floating-frame))
+
+(defun exwm-floatmode--mouse-click (&optional pos button-num window-id)
   "Perform a mouse click at position POS.
 
 POS can be an (x . y) cons pair, nil to click at the current
@@ -163,85 +232,70 @@ is the currently selected window."
     (xcb:flush exwm--connection)
     xy-pos))
 
-(defun exwm-picture-in-picture--get-nonfloating-window ()
+(defun exwm-floatmode--get-nonfloating-window ()
   "Get the first window in a non-floating buffer."
   (car (window-list exwm-workspace--current)))
 
-;; (defvar exwm-picture-in-picture--video-toggle nil
+;; (defvar exwm-floatmode--video-toggle nil
 ;;   "The video play/pause state.")
 
 ;;;###autoload
-(defun exwm-picture-in-picture-toggle-video ()
+(defun exwm-floatmode-toggle-video ()
   "Toggle the play/pause state of the video.
 It assumes the first tabbed position would yield the play/pause button."
   (interactive)
-  (let ((curr-win (get-buffer-window (current-buffer) t))
-        (float-win (exwm-picture-in-picture--get-floating-window)))
-    (if (eq curr-win float-win)
-        ;; Select the first window in the current frame
-        (setq curr-win (exwm-picture-in-picture--get-nonfloating-window)))
-    (if (not float-win)
-        ;; if no window, disable the move mode
-        (if exwm-picture-in-picture-move-mode (exwm-picture-in-picture-move-mode -1))
-      (select-window float-win)
-      ;; -- The below commented section does not work reliably
-      ;; (setq exwm-picture-in-picture--video-toggle (not exwm-picture-in-picture--video-toggle))
-      ;; (exwm-input--fake-key
-      ;;  (if exwm-picture-in-picture--video-toggle 'XF86AudioPause 'XF86AudioPlay))
-      (exwm-input--fake-key 'escape)
-      (exwm-picture-in-picture--mouse-click 'center 2) ;; middle click
-      (exwm-input--fake-key 'tab)
-      (exwm-input--fake-key 'return)
-      (exwm-input--fake-key 'S-tab) ;; reset button position
-      (select-window curr-win))))
+  (exwm-floatmode--do-floatfunc-and-restore
+   (lambda (curr-win float-win)
+     ;; -- The below commented section does not work reliably
+     ;; (setq exwm-floatmode--video-toggle (not exwm-floatmode--video-toggle))
+     ;; (exwm-input--fake-key
+     ;;  (if exwm-floatmode--video-toggle 'XF86AudioPause 'XF86AudioPlay))
+     (exwm-input--fake-key 'escape)
+     (exwm-floatmode--mouse-click 'center 2) ;; middle click
+     (exwm-input--fake-key 'tab)
+     (exwm-input--fake-key 'return)
+     (exwm-input--fake-key 'S-tab)))) ;; reset button position
 
-(defun exwm-picture-in-picture--move (direction)
-  "Move floating frame in DIRECTION symbol: left, right, up, down."
-  (if (not (exwm-picture-in-picture--get-floating-buffer))
-      (if exwm-picture-in-picture-move-mode (exwm-picture-in-picture-move-mode -1))
-    ;; The mode is active in the calling buffer window, but the
-    ;; floating window needs to be selected to work.
-    (let ((curr-win (get-buffer-window (current-buffer)))
-          (float-win (get-buffer-window exwm-picture-in-picture---float-buffer t)))
-      (if (eq curr-win float-win)
-          ;; Select the first window in the current frame
-          (setq curr-win (exwm-picture-in-picture--get-nonfloating-window))
-        (select-window float-win)
-        ;; (with-current-buffer exwm-picture-in-picture---float-buffer
-        (cond ((eq direction 'left) (exwm-floating-move (- exwm-picture-in-picture-move-amount) 0))
-              ((eq direction 'right) (exwm-floating-move exwm-picture-in-picture-move-amount 0))
-              ((eq direction 'up) (exwm-floating-move 0 (- exwm-picture-in-picture-move-amount)))
-              ((eq direction 'down) (exwm-floating-move 0 exwm-picture-in-picture-move-amount))
-              (t (user-error "No such direction"))))
-      (select-window curr-win))))
+(defun exwm-floatmode-move (direction &optional amount)
+  "Move floating frame by AMOUNT in DIRECTION symbol: left, right, up, down."
+  (interactive (list (read-key "Direction ")))
+  (let ((amount (or amount (plist-get exwm-floatmode-modify-amount :move))))
+    (exwm-floatmode--do-floatfunc-and-restore
+     (lambda (a b)
+       (cond ((eq direction 'left) (exwm-floating-move (- amount) 0))
+             ((eq direction 'right) (exwm-floating-move amount 0))
+             ((eq direction 'up) (exwm-floating-move 0 (- amount)))
+             ((eq direction 'down) (exwm-floating-move 0 amount))
+             (t (user-error "No such direction")))))))
 
-(defun exwm-picture-in-picture-resize-delta (&optional deltax deltay)
+(defun exwm-floatmode-resize-delta (&optional deltax deltay)
   "Resize the floating window by DELTAX pixels right and DELTAY pixels down.
 
 Both DELTAX and DELTAY default to 1.  This command should be bound locally."
-  (exwm--log "DeltaX: %s, DeltaY: %s" deltax deltay)
-  (unless (and (derived-mode-p 'exwm-mode) exwm--floating-frame)
-    (user-error "[EXWM] `exwm-floating-move' is only for floating X windows"))
-  (unless deltax (setq deltax 1))
-  (unless deltay (setq deltay 1))
-  (unless (and (= 0 deltax) (= 0 deltay))
-    (let* ((floating-container (frame-parameter exwm--floating-frame
-                                                'exwm-container))
-           (geometry (xcb:+request-unchecked+reply exwm--connection
-                         (make-instance 'xcb:GetGeometry
-                                        :drawable floating-container)))
-           (edges (window-inside-absolute-pixel-edges)))
-      (with-slots (width height) geometry
-        (exwm--set-geometry floating-container nil nil
-                            (+ width deltax) (+ height deltay))
-        (exwm--set-geometry exwm--id nil nil
-                            (+ width deltax) (+ height deltay))
-      (xcb:flush exwm--connection)))))
+  (exwm-floatmode--do-floatfunc-and-restore
+   (lambda (cwin fwin)
+     (exwm--log "DeltaX: %s, DeltaY: %s" deltax deltay)
+     (unless (and (derived-mode-p 'exwm-mode) exwm--floating-frame)
+       (user-error "[EXWM] `exwm-floating-move' is only for floating X windows"))
+     (unless deltax (setq deltax 1))
+     (unless deltay (setq deltay 1))
+     (unless (and (= 0 deltax) (= 0 deltay))
+       (let* ((floating-container (frame-parameter exwm--floating-frame
+                                                   'exwm-container))
+              (geometry (xcb:+request-unchecked+reply exwm--connection
+                            (make-instance 'xcb:GetGeometry
+                                           :drawable floating-container)))
+              (edges (window-inside-absolute-pixel-edges)))
+         (with-slots (width height) geometry
+           (exwm--set-geometry floating-container nil nil
+                               (+ width deltax) (+ height deltay))
+           (exwm--set-geometry exwm--id nil nil
+                               (+ width deltax) (+ height deltay))
+           (xcb:flush exwm--connection)))))))
 
-(defun exwm-picture-in-picture-resize (&optional width height)
-  "Resize the floating window by WIDTH pixels and down by HEIGHT pixels.
-
-Both WIDTH and HEIGHT default to 1.  This command should be bound locally."
+(defun exwm-floatmode-resize (width height)
+  "Resize the floating window to WIDTH pixels and to HEIGHT pixels."
+  (interactive "nWidth: \nnHeight: ")
   (exwm--log "width: %s, height: %s" width height)
   (unless (and (derived-mode-p 'exwm-mode) exwm--floating-frame)
     (user-error "[EXWM] `exwm-floating-move' is only for floating X windows"))
@@ -254,12 +308,8 @@ Both WIDTH and HEIGHT default to 1.  This command should be bound locally."
       (exwm--set-geometry exwm--id nil nil width height)
       (xcb:flush exwm--connection))))
 
-
-;; (defun center-click-current-window ()
-;;   (exwm-picture-in-picture--mouse-click 'center 2))
-
 ;;;###autoload
-(defun exwm-picture-in-picture-pause-media-windows (&optional matchstr num)
+(defun exwm-floatmode-pause-media-windows (&optional matchstr num)
   "Pause all media windows matching regex MATCHSTR, and limit to the first NUM.
 
 If MATCHSTR is nil, default to ``.*[Ww]atch.*''.  If NUM is nil, limit to none."
@@ -283,10 +333,10 @@ If MATCHSTR is nil, default to ``.*[Ww]atch.*''.  If NUM is nil, limit to none."
                    (setq toggled-windows (concat toggled-windows
                                                  (format "'%s %s "
                                                          exwm-title
-                                                         (exwm-picture-in-picture--mouse-click 'center 1 wid))))
+                                                         (exwm-floatmode--mouse-click 'center 1 wid))))
                    (setq num (1- num))))))))
-    (exwm-picture-in-picture-toggle-video)
+    (exwm-floatmode-toggle-video)
     (message "Toggled: %s" toggled-windows)))
 
-(provide 'exwm-picture-in-picture)
-;;; exwm-picture-in-picture.el ends here
+(provide 'exwm-floatmode)
+;;; exwm-floatmode.el ends here
